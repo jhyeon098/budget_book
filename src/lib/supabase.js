475b32be3c4948ca1af923aafb0237941e71825e
Ju hyeon Lee <juhyeonlee@joohyeonleeui-MacBookPro.local>
2026-05-06 19:06:41 +0900

    modified

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  import.meta.env.VITE_SUPABASE_URL || 'https://fwzgvtralwreqwmotuzm.supabase.co',
+  import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3emd2dHJhbHdyZXF3bW90dXptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNTM4NzMsImV4cCI6MjA5MzYyOTg3M30.jlxhW8aUJ-G718K9WKKoppY7h6d8yp3ez0A0k3Rl3PQ'
 )
